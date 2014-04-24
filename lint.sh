0fadf017efba067d37e08fbea4604bf587da08ec
Justin Phelps <linuturk@onitato.com>
2014-04-24 15:24:01 -0500

    Forgot the directory

--- lint.sh
+++ lint.sh
@@ -6,5 +6,5 @@ for dir in defaults handlers meta tasks vars
 do
     # Basic YAML syntax checking script
     echo "===== Checking $i directory ====="
-    python syntax_check.py role/$i/*.yml
+    python test.ping/syntax_check.py role/$i/*.yml
 done
