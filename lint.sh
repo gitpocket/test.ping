69e004e9f2757ebe5cdd46909cbc09616dfbce4d
Justin Phelps <linuturk@onitato.com>
2014-04-24 14:08:47 -0500

    Writing the necessary setup for the ansible-lint script.

---
+++ lint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+# Clone the ansible-lint repository
+rm -rf ansible-lint
+git clone git@github.com:willthames/ansible-lint.git
+
+export PYTHONPATH=$PYTHONPATH:`pwd`/ansible-lint/lib
