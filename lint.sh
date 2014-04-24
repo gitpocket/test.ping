4fb73799c070f848e85cb3cd424d508e2836aa33
Justin Phelps <linuturk@onitato.com>
2014-04-24 14:53:43 -0500

    Removing some complication

--- lint.sh
+++ lint.sh
@@ -2,14 +2,14 @@
 
 set -e
 
+# Pull in the ansible-lint project
 echo "===== Cloning Ansible-Lint Project ====="
-echo 'git clone https://github.com/willthames/ansible-lint.git "$WORKSPACE/ansible-lint"'
 git clone https://github.com/willthames/ansible-lint.git "$WORKSPACE/ansible-lint"
 
 # Setup path for ansible-lint command
 echo "===== Setting PYTHONPATH ====="
 export PYTHONPATH=$PYTHONPATH:"$WORKSPACE/ansible-lint/lib"
 
-# invoke the test
+# Invoke the test
 echo "===== Invoking Test ====="
-"python $WORKSPACE/ansible-lint/bin/ansible-lint" "$WORKSPACE/ansible-lint/examples/play.yml"
+python ansible-lint/bin/ansible-lint ansible-lint/examples/play.yml
