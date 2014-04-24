6917c3a6fdf1a977e2d9967fbbeb79c0a74f4c4d
Justin Phelps <linuturk@onitato.com>
2014-04-24 15:23:15 -0500

    Ripping out ansible-lint for now.

--- lint.sh
+++ lint.sh
@@ -2,16 +2,9 @@
 
 set -e
 
-# Pull in the ansible-lint project
-echo "===== Cloning Ansible-Lint Project ====="
-git clone https://github.com/willthames/ansible-lint.git "$WORKSPACE/ansible-lint"
-
-# Setup path for ansible-lint command
-echo "===== Setting PYTHONPATH ====="
-echo $PYTHONPATH
-export PYTHONPATH=$PYTHONPATH:`pwd`/ansible-lint/lib
-echo $PYTHONPATH
-
-# Invoke the test
-echo "===== Invoking Test ====="
-python ansible-lint/bin/ansible-lint ansible-lint/examples/play.yml
+for dir in defaults handlers meta tasks vars
+do
+    # Basic YAML syntax checking script
+    echo "===== Checking $i directory ====="
+    python syntax_check.py role/$i/*.yml
+done
