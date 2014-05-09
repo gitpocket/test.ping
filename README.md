a653d62557f78b02517a6d52f365c2c0549d526a
Justin Phelps <linuturk@onitato.com>
2014-05-09 15:52:09 -0500

    Removing ping stuff, and just doing syntax checking.

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 test.ping
 =========
 
-A simple playbook to confirm hosts are responding.
-
-This also serves as a repository for our test playbooks that confirm functionality for the roles in this organization.
-
-[![Build Status](http://jenkins.onitato.com:8080/job/Ansible%20Roles/badge/icon)](http://jenkins.onitato.com:8080/job/Ansible%20Roles/)
+This repository contains some tools for syntax checking of our Ansible Roles.
--- lint.sh
+++ lint.sh
@@ -2,9 +2,17 @@
 
 set -e
 
-for dir in defaults handlers meta tasks vars
+DRONEDIR="/var/cache/drone/src/github.com/rack-roles/*"
+
+for dir in defaults handlers meta tasks vars tests
 do
     # Basic YAML syntax checking script
     echo "===== Checking $dir directory ====="
-    python test.ping/syntax_check.py role/$dir/*.yml
+    python test.ping/yaml_check.py $DRONEDIR/$dir/*.yml
+done
+for dir in templates
+do
+    # Basic YAML syntax checking script
+    echo "===== Checking $dir directory ====="
+    python test.ping/jinja_check.py $DRONEDIR/$dir/*.j2
 done
