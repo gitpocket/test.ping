c658b0ccadde330839bdb6eea0bdd05791334d63
Justin Phelps <linuturk@onitato.com>
2014-05-20 11:31:02 -0500

    should be the final version of the new syntax check

--- jinja_check.py
+++ jinja_check.py
@@ -3,7 +3,7 @@ import jinja2
 
 path = sys.argv[1]
 loader = jinja2.FileSystemLoader(path)
-j2files = loader.list_templates()
+files = loader.list_templates()
 
 def dummy():
     '''
@@ -11,10 +11,20 @@ def dummy():
     '''
     pass
 
+# Setup env and custom filters
 env = jinja2.Environment(loader=loader)
 env.filters['mandatory'] = dummy
 
+# exit 0 unless changed below
 exit = 0
+
+# strip out all files that don't end in .j2
+j2files = []
+for f in files:
+    if f.endswith('.j2'):
+        j2files.append(f)
+
+# perform the syntax check on the j2 files
 for f in j2files:
     try:
         template = env.get_template(f)
--- tests/test.sh
+++ tests/test.sh
@@ -7,12 +7,12 @@ DRONEDIR="/var/cache/drone/src/github.com/rack-roles/test.ping"
 for dir in tests
 do
     # Basic YAML syntax checking script
-    echo "===== Checking $dir directory ====="
+    echo "===== Checking YAML in $dir ====="
     python $DRONEDIR/yaml_check.py $DRONEDIR/$dir/*.yml
 done
 for dir in tests
 do
-    # Basic YAML syntax checking script
-    echo "===== Checking $dir directory ====="
+    # Basic Jinja syntax checking script
+    echo "===== Checking Jinja in $dir ====="
     python $DRONEDIR/jinja_check.py $DRONEDIR/$dir
 done
