4f8ae9779936be7261bbef8917024bec2128ebf5
Justin Phelps <linuturk@onitato.com>
2014-05-20 11:15:58 -0500

    looks like the custom filter stuff might be working

--- jinja_check.py
+++ jinja_check.py
@@ -2,7 +2,8 @@ import sys
 import jinja2
 
 path = sys.argv[1]
-j2files = sys.argv[2:]
+loader = jinja2.FileSystemLoader(path)
+j2files = loader.list_templates()
 
 def dummy():
     '''
@@ -10,7 +11,7 @@ def dummy():
     '''
     pass
 
-env = jinja2.Environment(loader=jinja2.FileSystemLoader(path))
+env = jinja2.Environment(loader=loader)
 env.filters['mandatory'] = dummy
 
 exit = 0
@@ -18,9 +19,9 @@ for f in j2files:
     try:
         template = env.get_template(f)
     except (jinja2.TemplateSyntaxError) as e:
-        print '********\nError: %s\n********' % e
+        print '********\n%s\n********' % e
         exit += 1
     else:
-        print '%s: Syntax OK' % j2
+        print '%s: Syntax OK' % f
 
 sys.exit(exit)
