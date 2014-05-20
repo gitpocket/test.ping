9b510a533f1cab1d646e27158a6bf6289506b330
Justin Phelps <linuturk@onitato.com>
2014-05-20 10:58:01 -0500

    First round of the custom filter implementation.

--- jinja_check.py
+++ jinja_check.py
@@ -1,18 +1,22 @@
 import sys
 import jinja2
 
+def dummy():
+    '''
+    Simple dummy function to simulate custom filters.
+    '''
+    pass
+
+env = Environment(loader=FileSystemLoader(sys.argv[1]))
+env.filters['mandatory'] = dummy
 
 exit = 0
-for j2 in sys.argv[1:]:
-    try:
-        with open(j2) as f:
-            template = jinja2.Template(f.read())
-    except (jinja2.TemplateAssertionError) as e:
-        print '********\nWarning: %s\n********' % e
-    except (jinja2.TemplateSyntaxError) as e:
-        print '********\nError: %s\n********' % e
-        exit += 1
-    else:
-        print '%s: Syntax OK' % j2
+try:
+    template = env.get_template(sys.argv[2:])
+except (jinja2.TemplateSyntaxError) as e:
+    print '********\nError: %s\n********' % e
+    exit += 1
+else:
+    print '%s: Syntax OK' % j2
 
 sys.exit(exit)
