365ac9b246d78e4e82d41dea0efefbb44967c8d9
Justin Phelps <linuturk@onitato.com>
2014-05-20 09:47:08 -0500

    adding in tests for our tests. yo dawg. also typo in lint.sh

--- tests/test.sh
+++ tests/test.sh
@@ -4,15 +4,15 @@ set -e
 
 DRONEDIR="/var/cache/drone/src/github.com/rack-roles/*"
 
-for dir in defaults handlers meta tasks vars tests
+for dir in tests
 do
     # Basic YAML syntax checking script
     echo "===== Checking $dir directory ====="
     python test.ping/yaml_check.py $DRONEDIR/$dir/*.yml
 done
-for dir in templates
+for dir in tests
 do
-    # Basic Jinja syntax checking script
+    # Basic YAML syntax checking script
     echo "===== Checking $dir directory ====="
     python test.ping/jinja_check.py $DRONEDIR/$dir/*.j2
 done
