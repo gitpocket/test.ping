10f0d63e3b3b26069ccf9c73aa62f7694525a364
Justin Phelps <linuturk@onitato.com>
2014-05-20 11:19:20 -0500

    updated lint.sh

--- lint.sh
+++ lint.sh
@@ -7,12 +7,12 @@ DRONEDIR="/var/cache/drone/src/github.com/rack-roles/*"
 for dir in defaults handlers meta tasks vars tests
 do
     # Basic YAML syntax checking script
-    echo "===== Checking $dir directory ====="
+    echo "===== Checking YAML in $dir ====="
     python test.ping/yaml_check.py $DRONEDIR/$dir/*.yml
 done
 for dir in templates
 do
     # Basic Jinja syntax checking script
-    echo "===== Checking $dir directory ====="
+    echo "===== Checking Jinja in $dir ====="
     python test.ping/jinja_check.py $DRONEDIR/$dir
 done
