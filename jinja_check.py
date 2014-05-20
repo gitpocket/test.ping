b9072dc3f6b203a57b301e145a89d543e9ee7a28
Justin Phelps <linuturk@onitato.com>
2014-05-20 10:43:36 -0500

    testing for issue #7

--- jinja_check.py
+++ jinja_check.py
@@ -1,13 +1,16 @@
 import sys
 import jinja2
 
+
 exit = 0
 for j2 in sys.argv[1:]:
     try:
         with open(j2) as f:
-            jinja2.Template(f.read())
+            template = jinja2.Template(f.read())
+    except (jinja2.TemplateAssertionError) as e:
+        print '********\nWarning: %s\n********' % e
     except (jinja2.TemplateSyntaxError) as e:
-        print '********\n%s\n********' % e
+        print '********\nError: %s\n********' % e
         exit += 1
     else:
         print '%s: Syntax OK' % j2
