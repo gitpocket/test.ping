5c2baf363d3325d41029c7116c3eabbd2e600de2
Justin Phelps <linuturk@onitato.com>
2014-05-20 11:03:46 -0500

    adding list iteration

--- jinja_check.py
+++ jinja_check.py
@@ -1,22 +1,26 @@
 import sys
 import jinja2
 
+path = sys.argv[1]
+j2files = sys.argv[2:]
+
 def dummy():
     '''
     Simple dummy function to simulate custom filters.
     '''
     pass
 
-env = jinja2.Environment(loader=jinja2.FileSystemLoader(sys.argv[1]))
+env = jinja2.Environment(loader=jinja2.FileSystemLoader(path))
 env.filters['mandatory'] = dummy
 
 exit = 0
-try:
-    template = env.get_template(sys.argv[2:])
-except (jinja2.TemplateSyntaxError) as e:
-    print '********\nError: %s\n********' % e
-    exit += 1
-else:
-    print '%s: Syntax OK' % j2
+for f in j2files:
+    try:
+        template = env.get_template(f)
+    except (jinja2.TemplateSyntaxError) as e:
+        print '********\nError: %s\n********' % e
+        exit += 1
+    else:
+        print '%s: Syntax OK' % j2
 
 sys.exit(exit)
